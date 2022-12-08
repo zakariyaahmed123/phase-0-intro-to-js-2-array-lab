c71583e166b6ef940bd835c098a541308f10de5b
zakariyaahmed123 <118251771+zakariyaahmed123@users.noreply.github.com>
2022-12-08 10:20:47 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    return cats.push(name);
+}
+function destructivelyPrependCat(name) {
+    return cats.unshift(name);
+}
+function destructivelyRemoveLastCat() {
+    return cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+    return cats.shift();
+}
+function appendCat(name) {
+    const editedCat = [...cats, name];
+    return editedCat;
+}
+function prependCat(name) {
+    const editedCat = [name, ...cats];
+    return editedCat;
+}
+function removeLastCat() {
+    return cats.slice(0, cats.length - 1);
+}
+function removeFirstCat() {
+    return cats.slice(1);
+}
+
